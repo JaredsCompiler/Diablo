1c65cfa0c196703bbc5c2ed582d1dec560c5b437
JaredDyreson <jared.dyreson@gmail.com>
2020-09-12 23:28:16 -0700

    everything

--- lexical_analysis/sketches/word_t.py
+++ lexical_analysis/sketches/word_t.py
@@ -2,11 +2,21 @@
 
 import re
 
+class MessageException(Exception):
+    def __init__(self, message):
+        if not (isinstance(message, str)):
+            raise ValueError
+        self.message = message
+
+class LexingError(MessageException):
+    def __init__(self, message):
+        super().__init__(message)
+
 # assume string ignores whitespace
 
 filters = {
     "COMMENT": re.compile("(^\!.*\!)"),
-    "SEPARATORS": re.compile("(?!\s+)(\W+)"),
+    # "SEPARATORS": re.compile("(?!\s+)(\W+)"),
     "IDENTIFIER": re.compile("(\w+)(?!,?\s+\w+)*"),
     "KEYWORD": re.compile("(int|float|bool|true|false|(end)?if|else|then|while(end)?|do(end)?|for(end)?|(in|out)put|and|or|not)"),
     "OPERATORS": re.compile("(\+|-|\*|\/|=|>|<|>=|<=|&+|\|+|%|!|\^)")
@@ -17,6 +27,8 @@ class Lexeme():
         self.line_number = line_number
         self.begin, self.end = regex_match.span()
         self.tag = tag
+    def packed_repr(self):
+        return (self.line_number, self.begin, self.end)
 
     def __repr__(self):
         return f"""
@@ -29,17 +41,29 @@ with open("../inputs/source.txt", "r") as fp:
     contents = fp.readlines()
 
 collection = []
+
+max_passes = len(list(filters.keys()))
+
+def parseLine(string: str, lineno: int):
+    counter = 0
+    while(string and counter < max_passes):
+        for expression in filters:
+            match = filters[expression].match(string)
+
+            if(match):
+                x, y = match.span()
+                string = re.sub(string[x:y], '', string)
+                collection.append(Lexeme(expression, lineno, match))
+            else:
+                counter+=1
+    print(string)
+    # if(string):
+        # raise LexingError(f'String \"{string}\" still contains expressions that could not be identified')
+
 for x, line in enumerate(contents):
-    for expression in filters:
-        match = filters[expression].match(line)
-
-        if(match):
-            collection.append(Lexeme(expression, x, match))
-            if(expression == "COMMENT"):
-                break
-
+    parseLine(line.strip(), x)
 for element in collection:
     print(element)
-    l, s, e = element.line_number, element.begin, element.end
-    string = contents[l][s:e].replace(" ", "\s").replace("\n", "\\n").strip()
-    print(string)
+    lineno, begin, end = element.packed_repr()
+    print(f'Substring: {contents[lineno][begin:end]}')
+

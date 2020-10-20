a904304d2a77bbe54e4312acdfbcbb7efb4b75cf
JaredDyreson <jared.dyreson@gmail.com>
2020-10-20 14:02:53 -0700

    added pointer notation so we edit memory inline

--- syntax_analysis/sketches/stack_operations.cpp
+++ syntax_analysis/sketches/stack_operations.cpp
@@ -28,34 +28,42 @@ T compute(T a, T b, char s){
 }
 
 template <typename K>
-K calculate(std::stack<K> st_, std::stack<char> operands){
+K calculate(std::stack<K>* st_, std::stack<char>* operands){
 
   K inf = std::numeric_limits<K>::infinity();
 
-  if(st_.size() <= operands.size()){ return inf; }
+  if(st_->size() <= operands->size()){ return inf; }
 
-  while(!st_.empty() && !operands.empty()){
-    K op_1 = st_.top();
-    st_.pop();
-    if(st_.empty()){
-      st_.push(op_1);
+  while(!st_->empty() && !operands->empty()){
+    K op_1 = st_->top();
+    st_->pop();
+    if(st_->empty()){
+      st_->push(op_1);
       break;
     }
-    K op_2 = st_.top();
-    st_.pop();
+    K op_2 = st_->top();
+    st_->pop();
 
-    char operator_ = operands.top();
-    operands.pop();
+    char operator_ = operands->top();
+    operands->pop();
 
     K val = compute(op_1, op_2, operator_);
 
     if(val == inf){
       return val;
     } else {
-      st_.push(val);
+      st_->push(val);
     }
   }
-  return st_.top();
+  return st_->top();
+}
+
+template <typename T>
+
+T get_head(std::stack<T>* s){
+  T val = s->top();
+  s->pop();
+  return val;
 }
 
 int main(){
@@ -67,7 +75,7 @@ int main(){
   for(int i = 0; i < 9; ++i){
     opr_.push('+');
   }
-  size_t resultant = calculate(st_, opr_);
+  size_t resultant = calculate(&st_, &opr_);
   if(resultant == std::numeric_limits<size_t>::infinity()){
     std::cerr << "this has failed" << std::endl;
   } else {

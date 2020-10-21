437f8dbfcd984cfea422b3b04ff9295eb67955de
JaredDyreson <jared.dyreson@gmail.com>
2020-10-21 13:20:17 -0700

    updated switch statements

--- syntax_analysis/sketches/stack_operations.cpp
+++ syntax_analysis/sketches/stack_operations.cpp
@@ -2,33 +2,108 @@
 #include <limits>
 #include <stack>
 
+enum compare_code {
+  eLess,
+  eGreater,
+  eLessEqual,
+  eGreaterEqual,
+  eExplicitCompare,
+  eDoubleAnd,
+  eDoubleOr,
+  eCompNotFound
+};
+
+enum arthimetic_code {
+  eAdd,
+  eSub,
+  eDiv,
+  eMul,
+  eMod,
+  eInc,
+  eDec,
+  eMathNotFound
+};
+
+compare_code compareHash(std::string str){
+  if(str == ">"){ return eGreater; }
+  if(str == "<"){ return eLess; }
+  if(str == ">="){ return eGreaterEqual; }
+  if(str == "<="){ return eLessEqual; }
+  if(str == "=="){ return eExplicitCompare; }
+  if(str == "&&"){ return eDoubleAnd; }
+  if(str == "||"){ return eDoubleOr; }
+  return eCompNotFound;
+}
+
+arthimetic_code mathHash(std::string str){
+  if(str == "+"){ return eAdd; }
+  if(str == "-"){ return eSub; }
+  if(str == "/"){ return eDiv; }
+  if(str == "*"){ return eMul; }
+  if(str == "%"){ return eMod; }
+  if(str == "++"){ return eInc; }
+  if(str == "--"){ return eDec; }
+  return eMathNotFound;
+}
+
 template <typename T>
-T compute(T a, T b, char s){
+T compute(T a, T b, std::string s){
   // set to inf
   T val = std::numeric_limits<T>::infinity();
-  switch(s){
-    case '+':
-      val = a + b;
+  switch(mathHash(s)){
+    case eAdd:
+      val = (a + b);
       break;
-    case '-':
-      val = a - b;
+    case eSub:
+      val = (a - b);
       break;
-    case '/':
-      if(b == 0){
-        std::cerr << "cannot do that!" << std::endl;
-        break;
-      }
-      val = a / b;
+    case eDiv:
+      if(b == 0){ break; }
+      val = (a / b);
       break;
-    case '*':
-      val = a * b;
+    case eMul:
+      val = (a * b);
+      break;
+    case eMod:
+      val = (a % b);
+      break;
+    //case eInc:
+      //val = a
+  }
+  return val;
+}
+
+template <typename T>
+bool compare(T a, T b, std::string c){
+  T val = std::numeric_limits<bool>::infinity();
+  switch(compareHash(c)){
+    case eGreater:
+      val = (a > b);
+      break;
+    case eLess:
+      val = (a < b);
+      break;
+    case eGreaterEqual:
+      val = (a >= b);
+      break;
+    case eLessEqual:
+      val = (a <= b);
+      break;
+    case eDoubleAnd:
+      val = (a && b);
+      break;
+    case eDoubleOr:
+      val = (a || b);
+      break;
+    case eExplicitCompare:
+      val = (a == b);
       break;
   }
   return val;
 }
 
 template <typename K>
-K calculate(std::stack<K>* st_, std::stack<char>* operands){
+K calculate(std::stack<K>* st_, std::stack<std::string>* operands){
 
   K inf = std::numeric_limits<K>::infinity();
 
@@ -44,7 +119,7 @@ K calculate(std::stack<K>* st_, std::stack<char>* operands){
     K op_2 = st_->top();
     st_->pop();
 
-    char operator_ = operands->top();
+    std::string operator_ = operands->top();
     operands->pop();
 
     K val = compute(op_1, op_2, operator_);
@@ -58,22 +133,15 @@ K calculate(std::stack<K>* st_, std::stack<char>* operands){
   return st_->top();
 }
 
-template <typename T>
-
-T get_head(std::stack<T>* s){
-  T val = s->top();
-  s->pop();
-  return val;
-}
-
 int main(){
   std::stack<size_t> st_;
-  std::stack<char> opr_;
+  std::stack<std::string> opr_;
+  
   for(size_t i = 0; i < 10; ++i){
     st_.push(i);
   }
   for(int i = 0; i < 9; ++i){
-    opr_.push('+');
+    opr_.push("+");
   }
   size_t resultant = calculate(&st_, &opr_);
   if(resultant == std::numeric_limits<size_t>::infinity()){

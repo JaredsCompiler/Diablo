87ad499c3062ce46b0efb8b1cff6f8e5dcb745ef
JaredDyreson <jared.dyreson@gmail.com>
2020-10-23 16:13:33 -0700

    removed binaries and sorted Exceptions class

--- syntax_analysis/sketches/custom_exceptions.cpp
+++ syntax_analysis/sketches/custom_exceptions.cpp
@@ -2,24 +2,24 @@
 #include <exception>
 
 class DiabloBaseException : public std::exception {
-  const char* message;
   const char* file;
-  int line;
   const char* func;
   const char* info;
+  const char* message;
+  int line;
 
   public:
     DiabloBaseException(const char* message_, const char* file_, int line_, const char* func_, const char* info_ ="") : message(message_),
       file(file_),
-      line(line_),
       func(func_),
-      info(info_) {}
+      info(info_),
+      line(line_) {}
    const char* get_file() const { return file; }
-   int get_line() const { return line; }
    const char* get_func() const { return func; }
    const char* get_info() const { return info; }
-
    const char* what() const throw() { return message; }
+   int get_line() const { return line; }
+
 };
 
 int main(){
